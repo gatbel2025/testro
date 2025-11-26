5097ea4c2f94315d46fdcfe11e0abf15aec14a73
Gatbel Gatluak <gatbelgatluak2025@gmail.com>
2025-11-26 14:29:09 +0200

    Add motivational message to firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,3 @@
 # Print out the text hellow world to the console
 print("Hello world")
+Print("Gatbel and Nyaluak will overcome all the adversaries and eventually will become a couple united in spirit and eternal love")
